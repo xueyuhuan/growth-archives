b8d74c9f8bbea7aa157a514e7f64b392c271ba12
xueyuhuan <501946152@qq.com>
2018-11-07 11:58:38 +0800

    增加token验证，删除ico，修改文档名

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ export default {
   },
   data(){
     return{
-
+      token:''
     }
   },
   computed:{
@@ -24,13 +24,35 @@ export default {
     },
   },
   created(){
-    this.$ajax.post('/api/desktop/getInfo')
-      .then(res => {
-        this.$store.commit('setData',{
-          name:'user',
-          data:res.data.data
-        });
+    this.$ajax.post('/api/desktop/getToken')
+      .then(res=>{
+        if (res.data.errcode === '0') {
+          this.$store.commit('setToken', res.data.data.CZDA_TOKEN);//在store.js中设置token
+        }
+        else {
+          this.$message.error(res.data.errmsg);
+        }
       });
+    this.getUserInfo();
+  },
+  methods:{
+    getUserInfo(){
+      this.$ajax.post('/api/desktop/getInfo')
+        .then(res => {
+          this.$store.commit('setData',{
+            name:'user',
+            data:res.data.data
+          });
+        });
+    },
+    //获取cookie
+    getCookie(name){
+      let arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+      if(arr=document.cookie.match(reg))
+        return decodeURIComponent(arr[2]);
+      else
+        return false;
+    },
   }
 }
 </script>
--- src/store.js
+++ src/store.js
@@ -5,12 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    token:'',
     user:{},//用户信息
   },
   mutations: {
     setData(state,data){
       state[data.name]=data.data;
     },
+    setToken(state,token){
+      state.token=token;
+      sessionStorage.token=token;
+    }
   },
   actions: {
 
